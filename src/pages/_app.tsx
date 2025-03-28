26a37f863b7a0bb130f9c7ff8b572456ab1f378a
Nicolas Lukestik <nicolaslukestik@gmail.com>
2025-03-28 13:32:31 -0300

    tsconfig changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,8 @@ import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from 'styled-components';
-import theme from '../src/theme';
-import GlobalStyle from '../globalstyles';
+import theme from '@/theme';
+import GlobalStyle from 'globalstyles';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -26,6 +26,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head />
+        <meta name='description' content='Patients Dashboard' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
         <link rel='icon' href='/favicon.ico' />
         <meta name='theme-color' content='#ffffff' />
